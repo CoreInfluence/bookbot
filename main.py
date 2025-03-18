d4637281f3a2f270838811055ddec0fa9814af31
CoreInfluence <mailermailspartygifts@proton.me>
2025-03-18 19:41:04 -0400

    Final product

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 
-from stats import get_num_words
+import stats
+import sys
 
-def get_book_test(bookname):
-    book_path = f"books/{bookname}.txt"
+def get_book_test(book_path):
+    # book_path = f"books/{bookname}.txt"
     # print(book_path)
     with open(book_path,"r") as book:
         book_contents = book.read()
         return book_contents
 
-def main():
-    #print(get_book_test("frankenstein"))
-    print(f"{get_num_words('frankenstein')} words found in the document")
+def print_report(book, word_count, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        for k,v in item.items():
+            if k.isalpha():
+                print(f"{k}: {v}")
 
+def main():
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    # print(get_book_test(book))
+    num_words = stats.get_num_words(book)
+    # print(f"{num_words} words found in the document")
+    char_num_dict = stats.get_char_num(book)
+    # print(f"{char_num_dict}")
+    sorted_char_count_list = stats.sort_char_count(char_num_dict)
+
+    print_report(book, num_words, sorted_char_count_list)
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -1,8 +1,31 @@
-def get_num_words(bookname):
-    book_path = f"books/{bookname}.txt"
+def get_num_words(book_path):
+    # book_path = f"books/{bookname}.txt"
     # print(book_path)
     with open(book_path,"r") as book:
         book_contents = book.read()
         book_contents_list = book_contents.split()
         return len(book_contents_list)
-        # print(book_contents_list)
+
+def get_char_num(book_path):
+    # book_path = f"books/{bookname}.txt"
+    # print(book_path)
+    num_char_dict = {}
+    with open(book_path,"r") as book:
+        book_contents = book.read()
+        book_contents_list = book_contents.split()
+        for word in book_contents_list:
+            for char in word:
+                key = char.lower()
+                if key in num_char_dict:
+                    num_char_dict[f'{key}'] += 1
+                else:
+                    num_char_dict[f'{key}'] = 1
+
+        return num_char_dict
+def sort_char_count(num_char_dict):
+    sorted_dict_list = []
+    for key, value in dict(sorted(num_char_dict.items(), key=lambda item: item[1],reverse=True)).items():
+        key_dict = {key: value}
+        sorted_dict_list.append(dict(key_dict))
+
+    return sorted_dict_list
